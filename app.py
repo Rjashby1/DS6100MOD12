2232e156cd7380e3a7b36f6875b315c21681c9ca
Robert Judson Ashby <rjudsonashby@gmail.com>
2025-08-09 22:36:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -134,6 +134,5 @@ def update_graph(xaxis_col, color_col):
     fig.update_layout(transition_duration=500, plot_bgcolor='#222222', paper_bgcolor='#111111', font_color='#FFFFFF')
     return fig
 
-# --- Main entry point for Gunicorn ---
-if __name__ == '__main__':
-    app.run_server(debug=True)
+# This is for the production server
+server = app_final.server
